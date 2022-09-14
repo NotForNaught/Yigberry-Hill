e66c7f2d3e6a6f10e6c3fa90c1525415c0a29a64
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2022-09-14 18:17:30 -0400

    updated when to draw text box

--- TextBoxSprite.java
+++ TextBoxSprite.java
@@ -8,7 +8,6 @@ import java.io.File;
 import java.text.AttributedString;
 
 import javax.swing.ImageIcon;
-import javax.swing.JPanel;
 
 public class TextBoxSprite implements Sprite {
 
@@ -17,43 +16,36 @@ public class TextBoxSprite implements Sprite {
 	private boolean input = false;
 	private String words1 = "";
 	private String words2 = "";
-	JPanel j = new JPanel();
 	int counter = 1000;
 	char[] wordsanimation = null;
-	
-	@Override
-	public void update() {
-	}
-	
-	public void update(Input input, JPanel j) {
-		if (input.isSpacebar()) {
-			this.input = true;
-			this.j = j;
-			System.out.println("Space");
-			
-		} else if(input.isSpacebar() == false) {
-			this.input = false;
+	Font font = new Font("Victor's Pixel Font", Font.PLAIN, 30);
+	boolean on = false;
+	String message = "";
+
+	public void updateText(String s) {
+		if (on) {
+			on = false;
+			message = "";
+		} else {
+			on = true;
+			message = s;
 		}
 	}
-	Font font = new Font("Victor's Pixel Font", Font.PLAIN, 30);
+	
+	public void update() {
+	}
 
 	@Override
 	public void paint(Graphics g) {
-		if(input) {
+		if (on) {
 			g.drawImage(textbox.getImage(), 7, 290, 740, 225, null);
 			
 			g.setFont(font);
 			g.setColor(Color.BLACK);
-			g.drawString("you went to mailbox", 50, 490);
+			g.drawString(message, 50, 490);
 			
 			//g.drawString(words2, 50, 525);
-			
-			
 		} 
 	}
 
-	public void setInput(boolean input) {
-		this.input = input;
-	}
-	
 }
